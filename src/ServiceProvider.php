3d9b199e92e310737cbc23b51655c2fa7f41d8f2
Marat Dimaev <marat-dimaev@mail.ru>
2021-10-04 23:25:12 +0300

    refactor

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -16,13 +16,15 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/config.php', 'filesystems'
-        );
-
         Storage::extend('yandex-disk', function ($app, $config) {
             $client = new Disk($config['token']);
             return new Filesystem(new YandexDiskAdapter($client, $config['prefix']));
         });
+
+        app()->config['filesystems.disks.yandex-disk'] = [
+            'driver' => 'yandex-disk',
+            'token' => env('YANDEX_DISK_OAUTH_TOKEN'),
+            'prefix' => '/'.env('YANDEX_DISK_BASE_PATH', 'storage/'),
+        ];
     }
 }
